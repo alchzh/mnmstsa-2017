f645515307957e5230ea308705d260d0e4c1a11f
Jay Fault <alchzh@gmail.com>
2016-09-29 20:42:41 -0400

    Done stuff

--- game/main.ts
+++ game/main.ts
@@ -9,10 +9,13 @@ class SimpleGame {
     preload() {
         this.game.load.image('logo', 'phaser2.png');
     }
-    
+
     create() {
         var logo = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'logo');
+
         logo.anchor.setTo(0.5, 0.5);
+        logo.scale.setTo(0.2, 0.2);
+        this.game.add.tween(logo.scale).to({ x: 1, y: 1 }, 2000, Phaser.Easing.Bounce.Out, true);
     }
 }
 
